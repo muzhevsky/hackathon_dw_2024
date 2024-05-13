3094bc26c829b36612c26efca65e601a91cf139b
LmaoUpal <zemskovkostya2003@gmail.com>
2024-05-13 15:15:59 +0400

    error page; loading page

--- frontend/src/layouts/home/Home.tsx
+++ frontend/src/layouts/home/Home.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import LoadingPage from "../../pages/loadingPage/LoadingPage";
 
 const Home: React.FC = () => {
     return(
         <div>
             <h1>Home</h1>
+            <LoadingPage/>
             <Outlet></Outlet>
         </div>
     )
