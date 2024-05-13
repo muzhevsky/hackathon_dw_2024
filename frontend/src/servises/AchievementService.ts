1471a84f311832b9ebcde19ae7aaee2cb3d6d77d
LmaoUpal <zemskovkostya2003@gmail.com>
2024-05-14 02:36:55 +0400

    fix achievementForm

--- frontend/src/servises/AchievementService.ts
+++ frontend/src/servises/AchievementService.ts
@@ -4,16 +4,25 @@ import $api, { API_URL } from "../shared/utils/Api";
 
 class AchievementService{
     public static async create(dto: AchievementCreateDto){
-        return await $api.postForm(`${API_URL}/achievements`, dto, {
-            headers: {
-                'Content-Type': 'multipart/form-data'
-              },
-              withCredentials: true
-        })
+        try {
+            return await $api.postForm(`${API_URL}/achievements`, dto, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                },
+                withCredentials: true
+            })
+        } catch (e){
+            console.log(e);
+        }
     }
 
     public static async getAchievements(): Promise<Achievement[]>{
-        return (await $api.get<Achievement[]>(`${API_URL}/achievements`)).data;
+        try {
+            return (await $api.get<Achievement[]>(`${API_URL}/achievements`)).data;
+        } catch (e){
+            console.log(e);
+            return []
+        }
     }
 }
 
--- frontend/src/widgets/formLoadAchievements/FormLoadAchievements.tsx
+++ frontend/src/widgets/formLoadAchievements/FormLoadAchievements.tsx
@@ -1,4 +1,4 @@
-import { InboxOutlined } from "@ant-design/icons";
+import {CloseOutlined, InboxOutlined} from "@ant-design/icons";
 import { Upload } from "antd";
 import { RcFile } from "antd/es/upload";
 import { useState } from "react";
@@ -25,38 +25,40 @@ const FormLoadAchievements: React.FC<FormLoadAchievementsProps> = ({ closeHandle
     });
 
     //@ts-ignore
-    const dummyRequest = async ({ file, onSuccess }) => { 
-        const response = await AchievementService.create({File: file});   
-        console.log(response);
-        onSuccess("ok");
-        // setTimeout(() => {
-        //    onSuccess("ok");
-        // }, 0);
-      }
+    const dummyRequest = async ({ file, onSuccess }) => {
+        setTimeout(() => {
+            onSuccess("ok");
+        }, 0);
+    }
 
     return (
-        <div className={styles.DivForm}>
-            <button onClick={closeHandler}>Закрыть</button>
-            <Dragger 
-                name="file" 
-                multiple={false}
-                listType="picture"
-                //@ts-ignore
-                customRequest={dummyRequest}
-                beforeUpload={(file) => setFile(file)}>
-                <p className="ant-upload-drag-icon">
-                    <InboxOutlined />
-                </p>
-                <p className="ant-upload-text">Нажмите или перенесите файл для загрузки</p>
-            </Dragger>
-            {
-                isLoading
-                ? <FormForAchievement 
-                    data={data} closeHandler={closeHandler}/>
-                : <LoadingPage/>
-            }
+        <div className={styles.overlay}>
+            <div className={styles.container}>
+                <div>
+                    <CloseOutlined className={styles.closeElement} onClick={closeHandler}/>
+                    <Dragger
+                        name="file"
+                        multiple={false}
+                        listType="picture"
+                        //@ts-ignore
+                        customRequest={dummyRequest}
+                        beforeUpload={(file) => setFile(file)}>
+                        <p className="ant-upload-drag-icon">
+                            <InboxOutlined/>
+                        </p>
+                        <p className="ant-upload-text">Нажмите или перенесите файл для загрузки</p>
+                    </Dragger>
+                    {
+                        isLoading
+                            ? <FormForAchievement
+                                data={data} closeHandler={closeHandler}/>
+                            : <LoadingPage/>
+                    }
+                </div>
+            </div>
         </div>
     )
 }
+
 
 export default FormLoadAchievements;
