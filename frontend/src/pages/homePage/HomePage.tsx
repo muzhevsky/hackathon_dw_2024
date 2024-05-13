b107d2ad64b69f4eb5557dd61d78de8b14b330a6
katya <katya_yuneva@mail.ru>
2024-05-13 10:52:35 +0400

    create reroute in home page

--- frontend/src/pages/homePage/HomePage.tsx
+++ frontend/src/pages/homePage/HomePage.tsx
@@ -1,9 +1,30 @@
-import { useEffect } from "react";
-import { Outlet } from "react-router-dom"
+import { useContext, useEffect } from "react";
+import { Outlet, useLocation, useNavigate } from "react-router-dom"
+import { Context } from "../..";
+import { itemsNavigateDepartament } from "../../entities/role/RoleTabsDepartament";
+import { itemsNavigateStudent } from "../../entities/role/RoleTabsStudent";
+import { itemsNavigateTeacher } from "../../entities/role/RoleTabsTeacher";
+import { HOME_PATH } from "../../routing/RouterConstants";
 import NavigateTabs from "../../shared/ui/tabs/NavigateTabs";
 
 const HomePage: React.FC = () => {
-    //TODO сделать рероут на первую страницу массива табов в зависимости от роли
+    const navigate = useNavigate();
+    const locate = useLocation();
+    const { userStore } = useContext(Context);
+
+    useEffect(() => {
+        if(locate.pathname === HOME_PATH){
+            switch(userStore.activeRole?.title){
+                case "user":
+                    return navigate(itemsNavigateStudent[0]);
+                case "teacher":
+                    return navigate(itemsNavigateTeacher[0]);
+                case "departament":
+                    return navigate(itemsNavigateDepartament[0]);
+            }
+        }
+    }, [locate.pathname, userStore.activeRole])
+
 
     return(
         <>
