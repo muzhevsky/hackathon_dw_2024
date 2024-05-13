f515f8feeb7cd02985896fbecb20b33dbb115e9d
katya <katya_yuneva@mail.ru>
2024-05-14 02:44:31 +0400

    fix css

--- frontend/src/widgets/loginForm/LoginForm.tsx
+++ frontend/src/widgets/loginForm/LoginForm.tsx
@@ -1,26 +1,26 @@
-import {EyeInvisibleOutlined, EyeTwoTone} from "@ant-design/icons";
-import {Input} from "antd";
-import {observer} from "mobx-react-lite";
-import {useContext} from "react";
-import {useNavigate} from "react-router-dom";
-import {Context} from "../..";
+import { EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons";
+import { Input } from "antd";
+import { observer } from "mobx-react-lite";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { Context } from "../..";
 import { testUser } from "../../entities/user/User";
 import useInput from "../../hooks/UseInput";
-import {HOME_PATH} from "../../routing/RouterConstants";
+import { HOME_PATH } from "../../routing/RouterConstants";
 import AuthService from "../../servises/AuthService";
 import UserService from "../../servises/UserService";
 import PrimaryButton from "../../shared/ui/button/PrimaryButton";
-import {STORAGE_TOKEN, STORAGE_USER} from "../../shared/utils/StorageConstants";
+import { STORAGE_TOKEN, STORAGE_USER } from "../../shared/utils/StorageConstants";
 import styles from "./LoginForm.module.css";
 
 const LoginForm: React.FC = observer(() => {
-    const {userStore} = useContext(Context);
+    const { userStore } = useContext(Context);
     const navigate = useNavigate();
     const login = useInput();
     const password = useInput();
 
     const authHandler = async () => {
-        const response = await AuthService.login({login: login.value, password: password.value});
+        const response = await AuthService.login({ login: login.value, password: password.value });
         userStore.activeToken = response.token;
         const user = await UserService.GetUserById(response.user.id);
         userStore.user = user;
@@ -34,29 +34,30 @@ const LoginForm: React.FC = observer(() => {
         <div className={styles.FormWrap}>
             <h1 className={styles.loginHeader}>Авторизация</h1>
             <div className={styles.InpEnter}>
-                <label className={styles.LableText}>Логин</label>
+                {/* <label className={styles.LableText}>Логин</label> */}
                 <Input
                     size="large"
                     placeholder="Ваш логин..."
                     onChange={login.onChange}
                     className={styles.InputWrap}
                 />
-                <label className={styles.LableText}>Пароль</label>
+                {/* <label className={styles.LableText}>Пароль</label> */}
                 <Input.Password
                     size="large"
                     className={styles.InputWrap}
                     placeholder="Введите пароль..."
                     onChange={password.onChange}
-                    iconRender={(visible) => (visible ? <EyeTwoTone/> : <EyeInvisibleOutlined/>)}
+                    iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
                 />
-                <div className={styles.ButtonWrap}>
-                    <PrimaryButton
-                        content={"Войти"}
-                        clickHandler={authHandler}
-                        size={"large"}
-                    />
-                </div>
             </div>
+            <div className={styles.ButtonWrap}>
+                <PrimaryButton
+                    content={"Войти"}
+                    clickHandler={authHandler}
+                    size={"large"}
+                />
+            </div>
+
 
         </div>
     )
