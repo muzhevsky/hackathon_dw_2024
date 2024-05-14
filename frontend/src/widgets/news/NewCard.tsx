83bd7a44ef136f0f1cc54fd38d72b1dae0a99a83
LmaoUpal <zemskovkostya2003@gmail.com>
2024-05-14 09:50:02 +0400

    balance; news card popup add

--- frontend/src/widgets/news/NewCard.tsx
+++ frontend/src/widgets/news/NewCard.tsx
@@ -1,27 +1,44 @@
 import {New} from "../../entities/new/New";
 import styles from './NewCard.module.css'
 import Img from "../../shared/assets/sad.svg";
+import {useState} from "react";
+import Modal from "antd/lib/modal/Modal";
+import NewPopup from "./NewPopup";
 
 interface NewCardProps {
     newInfo: New;
 }
 
+
 const NewCard: React.FC<NewCardProps> = ({newInfo}) => {
+    const [modal2Open, setModal2Open] = useState(false);
     return (
-        <div className={styles.cardViev}>
-            <div className={styles.col}>
-                <p className={styles.date}>{newInfo.publicationDate.toLocaleDateString()}</p>
-                <div className={styles.row}>
-                    <img className={styles.imgSize} src={Img} alt=""/>
-                    <div className={styles.col}>
-                        <p className={styles.title}>{newInfo.title}</p>
-                        <p className={styles.description}>{newInfo.content}</p>
+        <>
+            {
+                modal2Open
+                    ? <NewPopup newInfo={newInfo} isOpen={modal2Open} setIsOpen={setModal2Open}/>
+                    : null
+            }
+            <div
+                className={styles.cardViev}
+
+            >
+                <div className={styles.col} onClick={() => {
+                    setModal2Open(true)
+                }}>
+                    <p className={styles.date}>{newInfo.publicationDate.toLocaleDateString()}</p>
+                    <div className={styles.row}>
+                        <img className={styles.imgSize} src={Img} alt=""/>
+                        <div className={styles.col}>
+                            <p className={styles.title}>{newInfo.title}</p>
+                            <p className={styles.description}>{newInfo.content}</p>
+                        </div>
                     </div>
                 </div>
+
+
             </div>
-
-
-        </div>
+        </>
     )
 }
 
