ecf8cc6fbaa436223a14885ca7a5bb9c897f9f58
andrew <andreimuzhevsky@gmail.com>
2024-05-14 18:15:14 +0400

    fixes

--- Backend/App/Program.cs
+++ Backend/App/Program.cs
@@ -1,9 +1,12 @@
+using System.Net.Mime;
 using Hackaton_DW_2024.App;
 using Hackaton_DW_2024.Data;
 using Hackaton_DW_2024.Data.Config;
 using Hackaton_DW_2024.Infrastructure.Auth;
 using Hackaton_DW_2024.Infrastructure.Logging;
 using Hackaton_DW_2024.Internal;
+using Hackaton_DW_2024.Internal.UseCases.Exceptions;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.Extensions.FileProviders;
 using ILogger = Hackaton_DW_2024.Infrastructure.Logging.ILogger;
 
@@ -43,6 +46,38 @@ app.UseCors(policy =>
 app.UseAuthentication();
 app.UseAuthorization();
 app.UseStaticFileServer(new StaticFileConfig());
+app.UseExceptionHandler(exceptionHandlerApp =>
+{
+    exceptionHandlerApp.Run(async context =>
+    {
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Response.ContentType = MediaTypeNames.Text.Plain;
+
+        await context.Response.WriteAsync("Internal server error.");
+
+        
+        var exceptionHandlerPathFeature =
+            context.Features.Get<IExceptionHandlerPathFeature>();
+        var exception = exceptionHandlerPathFeature?.Error;
+        if (exceptionHandlerPathFeature == null) return; 
+        
+        if (exception is EntityNotFoundException)
+            await context.Response.WriteAsync("NoSuchEntity");
+        
+        else if (exception is AuthException)
+        {
+            await context.Response.WriteAsync("Unauthorized");
+            await context.Response.WriteAsync(exception.ToString());
+            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+        }
+        
+        else if (exception is DuplicateEntityException)
+        {
+            await context.Response.WriteAsync("DuplicateEntity");
+            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+        }
+    });
+});
 
 app.MapControllers();
 app.Run();
